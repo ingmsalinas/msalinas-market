6963e9f52901a700c51c968b55438e23ccc2a414
msalinas <mario.salinas.arenas@gmail.com>
2021-05-01 16:58:01 -0500

    unversioned Files

---
+++ src/main/java/com/msalinas/market/MsalinasMarketApplication.java
@@ -0,0 +1,13 @@
+package com.msalinas.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsalinasMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsalinasMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msalinas/market/MsalinasMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msalinas.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsalinasMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
